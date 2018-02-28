8fc42412b3abb135784fa80116d7823884fc454c
sathish <sathishbabur@hotmail.com>
2018-02-28 13:27:03 +0530

    Added the Web site resources & pages with API authorization implementation.

---
+++ src/Controllers/ValuesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Addon365.WebSync.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        
+        // GET: api/<controller>
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/Startup.cs
@@ -0,0 +1,146 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json.Linq;
+
+namespace Addon365.WebSync
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = "Google";
+            })
+               .AddCookie()
+           .AddGoogle(googleOptions =>
+            {
+                googleOptions.ClientId = Configuration["Google:ClientId"];
+                googleOptions.ClientSecret = Configuration["Google:ClientSecret"];
+                googleOptions.CallbackPath = new PathString("/signin-github");
+                googleOptions.AuthorizationEndpoint = "https://accounts.google.com/o/oauth2/auth";
+                googleOptions.TokenEndpoint = "https://accounts.google.com/o/oauth2/token";
+
+            })
+    .AddJwtBearer(jwtBearerOptions =>
+    {
+        jwtBearerOptions.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateActor = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = Configuration["Issuer"],
+            ValidAudience = Configuration["Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["SigningKey"]))
+        };
+    });
+            services.AddTransient<IUrls, Urls>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                OnPrepareResponse =
+         r =>
+         {
+             string path = r.File.PhysicalPath;
+             if (path.EndsWith(".css") || path.EndsWith(".js") || path.EndsWith(".gif") || path.EndsWith(".jpg") || path.EndsWith(".png") || path.EndsWith(".svg"))
+             {
+                 TimeSpan maxAge = new TimeSpan(7, 0, 0, 0);
+                 r.Context.Response.Headers.Append("Cache-Control", "max-age=" + maxAge.TotalSeconds.ToString("0"));
+             }
+         }
+            });
+
+            app.UseAuthentication();
+
+            app.UseMvc(
+
+             routes =>
+             {
+                 routes.MapRoute(
+                  "SeoRoute",
+                  "{*permalink}",
+                 defaults: new { controller = "Software", action = "Index" },
+                 constraints: new { permalink = new CmsUrlConstraint(new Urls()) });
+
+                 routes.MapRoute(
+                     name: "default",
+                     template: "{controller=Home}/{action=Index}/{id?}");
+             }
+
+             );
+        }
+    }
+
+    public class CmsUrlConstraint : IRouteConstraint
+    {
+        private readonly IUrls _urls;
+        public CmsUrlConstraint(IUrls urls)
+        {
+            _urls = urls;
+        }
+        public bool Match(HttpContext httpContext, IRouter route, string routeKey, RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            var db = _urls.getUrls;
+            if (values[routeKey] != null)
+            {
+                var permalink = values[routeKey].ToString().ToLower();
+                var page = db.Where(p => p.Url == permalink).FirstOrDefault();
+                if (page != null)
+                {
+                    httpContext.Items["cmspage"] = page;
+                    return true;
+                }
+                return false;
+
+
+            }
+            return false;
+        }
+
+
+    }
+}
