16605e6a03f674919a9780dc4cf199f238fe92b3
sathish <sathishbabur@hotmail.com>
2018-02-28 19:57:42 +0530

    Updated the DB (Model) design

---
+++ Models/src/AuthorizeIdentity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Addon365.Models
+{
+    class AuthorizeIdentity
+    {
+        public Guid AuthorizeIdentityId { get; set; }
+
+        public License License { get; set; }
+
+        public string Provider { get; set; }
+
+        public string Identity { get; set; }
+    }
+}
